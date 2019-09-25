161188cc695391f200e9a30d97fdb0d057eb864e
Supriyo Mondal <hisupriyooo@gmail.com>
2019-09-25 10:31:46 +0530

    Updates

---
+++ precache-manifest.c1eda5dec36df18b549c76d9cb924d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359ca3921b6a37bbf40c20e21af4ba3e",
+    "url": "/material_ui_simple_form/index.html"
+  },
+  {
+    "revision": "fd9e1769f0def25ba7bb",
+    "url": "/material_ui_simple_form/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "ddb84cb717f8dabd2f9b",
+    "url": "/material_ui_simple_form/static/js/2.0912f41a.chunk.js"
+  },
+  {
+    "revision": "fd9e1769f0def25ba7bb",
+    "url": "/material_ui_simple_form/static/js/main.17d90c83.chunk.js"
+  },
+  {
+    "revision": "e98c69ad6e4ac5a13505",
+    "url": "/material_ui_simple_form/static/js/runtime-main.95270f82.js"
+  }
+]);
